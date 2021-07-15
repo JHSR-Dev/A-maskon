2d053e9982aa46e0f5f6d1e256b36afa1d397c52
Stephen Ro <stephenkro@gmail.com>
2021-07-15 17:33:12 -0400

    added some style changes to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,33 +1,54 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
-import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
+import "./NavBar.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+import ShoppingCartRoundedIcon from "@material-ui/icons/ShoppingCartRounded";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import { ReactComponent as FoxMaskIcon } from "./fox-mask.svg";
+import {makeStyles} from '@material-ui/core/styles';
+
+
+
+
+const useStyles = makeStyles((theme)=>({
+  appbar: {
+    color: 'secondary'
+  }
+  
+}))
+
+
 
 function NavBar() {
+  const classes = useStyles()
   return (
-    <AppBar position='static'>
-      <Toolbar>
-        <div>
-          <Typography>
-            <h1>A-maskon</h1>
-          </Typography>
+    <AppBar position="static" className={classes.appbar}>
+      <Toolbar className="navbar">
+        <div className="nav-title">
+          <Typography>A-maskon</Typography>
         </div>
-        <div>
-          <SearchIcon />
-          <InputBase placeholder='Find your mask...' />
-        </div>
-        <div>
-          <HomeRoundedIcon />
-        </div>
-        <div>
-          <ShoppingCartRoundedIcon />
-        </div>
-        <div>
-          <AccountCircleRoundedIcon />
+          <div>
+            <SearchIcon />
+            <InputBase placeholder="Find your mask..." />
+          </div>
+        <div className="navicons">
+         
+          <div>
+            <HomeRoundedIcon />
+          </div>
+          <div>
+            <SvgIcon component={FoxMaskIcon} viewBox="0 0 600 476.6" />
+          </div>
+          <div>
+            <ShoppingCartRoundedIcon />
+          </div>
+          <div>
+            <AccountCircleRoundedIcon />
+          </div>
         </div>
       </Toolbar>
     </AppBar>
