99ca3745daf1000ce398de7881f217c42f87054b
Stephen Ro <stephenkro@gmail.com>
2021-07-29 18:09:48 -0400

    feat(added firestore connection)

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import createMaskTable from '../firebase/firebaseMethods'
+
+
 const LandingPage = () => {
+  useEffect(()=>{
+    createMaskTable()
+  })
   return (
     <div>
       <h1>Hello world</h1>
