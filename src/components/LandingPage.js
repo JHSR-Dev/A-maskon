a7618647df9a48ba50f82e13761441e3a2b675d6
Jason Huang <jasonjh94@outlook.com>
2021-07-30 17:59:24 -0400

    added get mask firebase function

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,4 @@
-import { useEffect } from 'react';
-import createMaskTable from '../firebase/firebaseMethods'
-
-
 const LandingPage = () => {
-  useEffect(()=>{
-    createMaskTable()
-  })
   return (
     <div>
       <h1>Hello world</h1>
--- src/firebase/firebaseMethods.js
+++ src/firebase/firebaseMethods.js
@@ -1,17 +1,35 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+export default async function getMasks() {
+  try {
+    const db = firebase.firestore();
+    const firstMaskRef = db.collection('masks').doc('firstMask');
+    const firstMaskDoc = await firstMaskRef.get();
 
-export default async function createMaskTable() {
-    try {
-      const db = firebase.firestore();
-      await db
-        .collection("masks")
-        .doc('secondMask')
-        .set({
-           price: 20,
-           type: 'creative'
-         });
-    } catch (err) {
-      console.log("There is something wrong!!!!");
+    if (!firstMaskDoc.exists) {
+      console.log('Nothing like this exists');
+    } else {
+      console.log('Document data: ', firstMaskDoc.data());
     }
+  } catch (err) {
+    console.log(err);
   }
+}
+
+/*
+
+var docRef = db.collection("cities").doc("SF");
+
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+*/
