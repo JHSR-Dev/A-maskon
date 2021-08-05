3df9e324e66d67827e5057b61651b9325e12fab8
Stephen Ro <stephenkro@gmail.com>
2021-08-05 16:02:18 -0400

    added material ui for all masks component

--- src/components/Masks.js
+++ src/components/Masks.js
@@ -2,10 +2,29 @@ import { useEffect, useState } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "./Masks.css";
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { red, blue, green } from '@material-ui/core/colors';
 
-// import getMasks from '../firebase/firebaseMethods';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxHeight:600,
+    maxWidth:600,
+    backgroundColor: blue[100], 
+  },
+  media: {
+    height: 300,
+    width: 250,
+  },
+});
 
 const Masks = () => {
+  const classes = useStyles();
   const [mask, setMask] = useState([]);
 
   useEffect(() => {
@@ -27,8 +46,18 @@ const Masks = () => {
       <div className="allMasks">
         {mask.map((maskObj) => {
           return (
-            <div key={maskObj.id}>
-              <img src={maskObj.imageUrl} alt='' />
+            <div key={mask.id}>
+             <Card className ={classes.root}>
+              <CardMedia className={classes.media} image={maskObj.imageUrl} />
+              <CardContent>
+                <Typography>
+              {maskObj.name}
+              </Typography>
+              <Typography>
+              ${maskObj.price}
+              </Typography>
+              </CardContent>
+            </Card>
             </div>
           );
         })}
