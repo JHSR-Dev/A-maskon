e547d29fd13918e286364908cc2841f3c87ee22b
Stephen Ro <stephenkro@gmail.com>
2021-08-05 16:36:11 -0400

    more style changes on each card for the masks

--- src/components/Masks.js
+++ src/components/Masks.js
@@ -7,20 +7,29 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { red, blue, green } from '@material-ui/core/colors';
-
-
+import { blue } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import CardActions from '@material-ui/core/CardActions';
 
 const useStyles = makeStyles({
   root: {
     maxHeight:600,
     maxWidth:600,
-    backgroundColor: blue[100], 
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', 
+    border: 0,
+    borderRadius: 5,
+    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
   },
   media: {
     height: 300,
     width: 250,
   },
+  addToCart: {
+    padding: 10,
+    
+    
+  }
 });
 
 const Masks = () => {
@@ -42,7 +51,7 @@ const Masks = () => {
 
   return (
     <div>
-      <h1>Masks</h1>
+      <h1>Ready Set Mask Off</h1>
       <div className="allMasks">
         {mask.map((maskObj) => {
           return (
@@ -56,6 +65,15 @@ const Masks = () => {
               <Typography>
               ${maskObj.price}
               </Typography>
+              <CardActions>
+                <IconButton className={classes.addToCart}>
+                  <AddShoppingCartIcon />
+                  <Typography>
+                  Add to Cart
+                </Typography>
+                </IconButton>
+               
+              </CardActions>
               </CardContent>
             </Card>
             </div>
